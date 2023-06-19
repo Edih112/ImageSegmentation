f9ee6e81a4a236127d7bf955679d211a7430021a
edison.hao112@gmail.com <edison.hao112@gmail.com>
2023-06-19 12:28:40 -0700

    remove matrix FF

--- src/MaxFlow_test.py
+++ src/MaxFlow_test.py
@@ -5,36 +5,6 @@ from Dinics2 import Dinics2
 
 class MaxFlowTest(unittest.TestCase):
 
-    def test_getMaxFlow1(self):
-        matrix = np.array([[0,10,10,0,0,0],
-                       [0,0,2,4,8,0],
-                       [0,0,0,0,9,0],
-                       [0,0,0,0,0,10],
-                       [0,0,0,6,0,10],
-                       [0,0,0,0,0,0]])
-        mf = MaxFlowAdjMatrix(matrix, 0, 5)
-        self.assertEqual(mf.getMaxFlow(), 19)
-
-    def test_getMaxFlow2(self):
-        matrix = np.array([[0,16,13,0,0,0],
-                            [0,0,10,12,0,0],
-                            [0,4,0,0,14,0],
-                            [0,0,9,0,0,20],
-                            [0,0,0,7,0,4],
-                            [0,0,0,0,0,0]])
-        mf = MaxFlowAdjMatrix(matrix, 0, 5)
-        self.assertEqual(mf.getMaxFlow(), 23)
-
-    def test_getMaxFlow3(self):
-        matrix = np.array([[0,0,0,0,0,0],
-                           [0,0,41,0,0,0],
-                           [0,0,0,3,11,0],
-                           [0,0,0,0,0,0],
-                           [0,0,0,4,0,15],
-                           [0,0,4,21,0,0]])
-        mf = MaxFlowAdjMatrix(matrix, 1, 3)
-        self.assertEqual(mf.getMaxFlow(), 14)
-
     def test_getMaxFlowAdj1(self):
         adj_list = [{1:10, 2:10},
                     {2:2, 3:4, 4:8},
